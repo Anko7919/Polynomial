c6db8ad68b747d4eaf27e5fef89d9607539897d9
Anko7919 <hauptidealsatz.anko7919@gmail.com>
2025-09-09 00:27:46 +0900

    コンセプトを追加し，そのほか細かいところを修正

--- axioms.hpp
+++ axioms.hpp
@@ -8,55 +8,61 @@ namespace dorayaki {
     /* 四則演算を持っているか */
     // Addition
     template <typename T>
-    concept addable = requires (T &a, T &b) {
+    concept addable = requires (T a, T b) {
         { a + b } -> std::same_as<T>; 
     }; 
 
     // Subtraction
     template <typename T>
-    concept subtractable = requires (T &a, T &b) {
+    concept subtractable = requires (T a, T b) {
         { a - b } -> std::same_as<T>; 
     }; 
 
     // Multiplication
     template <typename T>
-    concept multipliable = requires (T &a, T &b) {
+    concept multipliable = requires (T a, T b) {
         { a * b } -> std::same_as<T>; 
     }; 
 
     // Division
     template <typename T>
-    concept divisible = requires (T &a, T &b) {
+    concept divisible = requires (T a, T b) {
         { a / b } -> std::same_as<T>; 
     }; 
 
     /* 単項演算子 */
     // Negative
     template <typename T>
-    concept negatable = requires (T &a) {
+    concept negatable = requires (T a) {
         { -a } -> std::same_as<T>; 
     }; 
 
     /* 単位元の存在 */
     // zero element
     template <typename T>
-    struct has_zero : public std::false_type {}
+    struct has_zero : public std::false_type {}; 
 
     template <typename T> requires std::is_arithmetic<T>
-    struct has_zero<T> : public std::true_type {}
+    struct has_zero<T> : public std::true_type {}; 
 
     template <typename T>
-    struct has_zero<std::complex<T>> : public std::true_type {}
+    struct has_zero<std::complex<T>> : public std::true_type {}; 
+
+    template <typename T>
+    concept zeroable = has_zero<T>::value; 
 
     // one element
     template <typename T>
-    struct has_one : public std::false_type {}
+    struct has_one : public std::false_type {};
 
     template <typename T> requires std::is_arithmetic<T>
-    struct has_one<T> : public std::true_type {}
+    struct has_one<T> : public std::true_type {}; 
 
     template <typename T>
-    struct has_one<std::complex<T>> : public std::true_type{}
+    struct has_one<std::complex<T>> : public std::true_type{}; 
+
+    template <typename T>
+    concept oneable = has_one<T>::value; 
 }
 
 #endif // ! DORAYAKI_AXIOMS_HPP_INCLUDED
