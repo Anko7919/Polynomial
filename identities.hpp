bccb62163e9e35d4d87af67c9e1d0058b0c0d599
Anko7919 <hauptidealsatz.anko7919@gmail.com>
2025-09-09 00:55:13 +0900

    zeroとoneを実装

--- identities.hpp
+++ identities.hpp
@@ -6,19 +6,33 @@
 
 namespace dorayaki {
     namespace utils {
-        template <typename T, typename <typename> Container>
+        template <typename T, template <typename> class Container>
         struct is_instance_of : public std::false_type {}; 
 
-        template <typename T, typename <typename> Container>
+        template <typename T, template <typename> class Container>
         struct is_instance_of<Container<T>, Container> : public std::true_type {}; 
     }
 
     template <zeroable T>
-    auto zero() noexcept -> T; 
+    constexpr auto zero() noexcept -> T {
+        constexpr auto b{ std::is_arithmetic_v<T> || utils::is_instance_of<T, std::complex>::value }; 
+        if constexpr (b) {
+            return static_cast<T>(0); 
+        }
+        else {
+            static_assert(b, "zero() is not defined for this type"); 
+        }
+    }
 
-    template <zeroable T> requires (std::is_arithmetic_v<T> || utils::is_instance_of<T, std::complex>)
-    auto zero() noexcept -> T {
-        return static_cast<T>(0); 
+    template <zeroable T>
+    constexpr auto one() noexcept -> T {
+        constexpr auto b{ std::is_arithmetic_v<T> || utils::is_instance_of<T, std::complex>::value }; 
+        if constexpr (b) {
+            return static_cast<T>(1); 
+        }
+        else {
+            static_assert(b, "one() is not defined for this type"); 
+        }
     }
 }
 
